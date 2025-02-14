e4c844644039459ca72cbd82e47e6f13093f39e4
wjwj910 <wjwj910@gmail.com>
2025-02-14 09:39:37 +0900

    람다

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,7 @@
 package org.example
 
 fun main() {
-    var name: String? = "HI"
-    name = "Hello"
-    // ? 붙히면 Strin 일수도 있고 null 일수도 있다.
-    name = null
-    
-    // String 이 있으면(null -> x) length 를 실행하고,
-    // 없으면 Name is null(null -> o) 을 실행하라
-    println(name?.length ?: "Name is null")
-    
+    val names = listOf("Alice", "Bob", "Charlie")
+
+    names.forEach { println("Hello, $it") }
 }
