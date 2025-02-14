09fc141c24c92a69fceb19fd073cbe611a5eb45a
wjwj910 <wjwj910@gmail.com>
2025-02-14 09:55:12 +0900

    문자열 템플릿

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,8 @@
 package org.example
 
-object Singleton {
-    fun showMessage() {
-        println("Hello from Singleton!")
-    }
-}
-
 fun main() {
-    Singleton.showMessage()
+    val name = "Alice"
+    val age = 25
+
+    println("My name is $name and I am $age years old.") // 문자열 템플릿 사용
 }
