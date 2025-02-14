6781ad32c92169ea2569a05e69ac442345cc7f9e
wjwj910 <wjwj910@gmail.com>
2025-02-14 09:57:11 +0900

    when 문

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,14 @@
 package org.example
 
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.StandardCopyOption
-
 fun main() {
-    copy("a.txt", "a_copy.txt")
-}
+    val day = 3
 
-fun copy(source: String, target: String) {
-    Files.copy(File(source).toPath(), File(target).toPath(), StandardCopyOption.REPLACE_EXISTING)
+    val dayName = when (day) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        else -> "Invalid Day"
+    }
+
+    println("Day: $dayName")
 }
