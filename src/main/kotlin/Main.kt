a3cd6bd20d5a90de14a0a8734c41134ca54afc05
wjwj910 <wjwj910@gmail.com>
2025-02-14 09:28:24 +0900

    클래스와 객체

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,12 @@
 package org.example
 
-fun main() {
-    val number = 10 // 불변 변수 선언 (val)
-    var message = "Hello, Kotlin!" // 문자열 변수 선언
-
-    println("Number: $number")
-    println("Message: $message")
-
-    val num = 15
-
-    val result = if (num % 2 == 0) "Even Number" else "Odd Number"
-    println(result)
-
-    for (i in 0 until 5) { // 0부터 4까지 반복
-        println("Count: $i")
+class Person(val name: String) {
+    fun greet() {
+        println("Hello, my name is $name")
     }
-
-    val result1 = add(5,10);
-    println("Result: $result1")
 }
 
-fun add(a: Int, b: Int): Int {
-    return a + b
+fun main() {
+    val person = Person("Alice")
+    person.greet()
 }
